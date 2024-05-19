4f85584f10531a8c40f1effc7482e55f6bfbe159
Muthu1629 <134788544+Muthu1629@users.noreply.github.com>
2024-05-19 18:26:09 +0530

    Updated Sidebar Component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
       {/* Sidebar */}
       <div className={`fixed inset-y-0 left-0 z-40 w-64 bg-gray-800 text-white transform transition-transform duration-300 h-screen ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:relative md:translate-x-0 md:w-64`}>
         <div className="flex items-center justify-between h-16 px-4 bg-gray-800 text-white md:h-auto">
-          <div className="flex items-center justify-between gap-4 h-full text-2xl font-bold pt-4"><img src={"/img/logo.png"} alt="Logo" className="h-8" /><span>CMS</span></div>
+          <div className="flex items-center justify-between gap-4 h-full text-2xl font-bold pt-4"><img src="./img/logo.png" alt="Logo" className="h-8" /><span>CMS</span></div>
           <div className="absolute right-[-26px] block md:hidden">
             <button onClick={() => setIsOpen(!isOpen)}>
               {isOpen ? <FaTimes className="h-6 w-6 text-gray-800" /> : <FaBars className="h-6 w-6 text-gray-800" />}
